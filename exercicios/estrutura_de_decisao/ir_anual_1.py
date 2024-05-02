df11a566dff58efacff3a3b3e3688c392c53d3bf
Fábio Florêncio <fabioflorencioxyz@gmail.com>
2024-05-01 21:47:37 -0300

    Mais exercícios

--- exercicios/estrutura_de_decisao/ir_anual_1.py
+++ exercicios/estrutura_de_decisao/ir_anual_1.py
@@ -1,6 +1,5 @@
-def calcular_ir(base_calc):
+def calcular_ir(base_calc,aliq_p_calc):
 
-   
     ALIQ_7_perc = 7
     ALIQ_15_perc = 15
     ALIQ_22_perc = 22.5
@@ -19,31 +18,36 @@ def calcular_ir(base_calc):
 
     REND_ACIMA = REND_MAX_22
 
+    # Isento
     if base_calc >= REND_ISENTO: 
         # Rendimento acima de R$ 53.565.72
-        if base_calc > REND_ACIMA:             
+        if base_calc > REND_ACIMA: 
+            deducao = (base_calc / 100) * ALIQ_27_perc
             aliq_p_calc = ALIQ_27_perc                        
         else:
             # Rendimento entre 21.453.24 a 32.151.48
-            if base_calc <= REND_MAX_7:                
+            if base_calc <= REND_MAX_7:
+                deducao = (base_calc / 100) * ALIQ_7_perc
                 aliq_p_calc = ALIQ_7_perc
             # Rendimento entre 32.151.49 a 42.869.16   
-            elif base_calc <= REND_MAX_15:                
+            elif base_calc <= REND_MAX_15:
+                deducao = (base_calc / 100) * ALIQ_15_perc
                 aliq_p_calc = ALIQ_15_perc  
             # Rendimento 42.869.17 a 53.565.72                                 
-            else:                                        
+            else:                        
+                deducao = (base_calc / 100) * ALIQ_22_perc
                 aliq_p_calc = ALIQ_22_perc                       
     else:
-        # Isento
         deducao = 0
         aliq_p_calc = ISENTO   
 
-    deducao = (base_calc / 100) * aliq_p_calc
+
 
     return deducao, aliq_p_calc 
 
 
-base_calc = 21453.23
-result_deducao,aliq_p_calc = calcular_ir(base_calc)         
+base_calc = 53565.72
+aliq_p_calc = 0
+result_deducao,aliq_p_calc = calcular_ir(base_calc,aliq_p_calc)         
 
 print('Dedução de R$%.2f com base da aliquota %.1f%%' %(result_deducao,aliq_p_calc))
---
+++ python_basico/funcao/exerc_11 copy.py
@@ -0,0 +1,63 @@
+'''
+    11 - As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram para desenvolver o 
+        programa que calculará os reajustes.
+        Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, 
+        baseado no salário atual:
+        salários até R$ 280,00 (incluindo) : aumento de 20%
+        salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+        salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+        salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+        o salário antes do reajuste;
+        o percentual de aumento aplicado;
+        o valor do aumento;
+        o novo salário, após o aumento.
+
+'''
+
+def reajustar_salario(sal_ant):
+    
+    PERC_20 = 20
+    PERC_15 = 15
+    PERC_10 = 10
+    PERC_5 = 5
+    
+    if sal_ant <= 280:
+        perc_aumento = PERC_20
+    elif sal_ant <= 700:
+        perc_aumento = PERC_15
+    elif sal_ant <= 1500:
+        perc_aumento = PERC_10
+    else:
+        perc_aumento = PERC_5
+
+    valor_aumento = (sal_ant / 100) * perc_aumento
+    sal_atual = sal_ant + valor_aumento
+
+    return sal_ant, sal_atual, perc_aumento, valor_aumento
+
+# A função retorna uma tupla com os valores da função
+result = reajustar_salario(700)
+msg = 'Salário antigo: R${:.2f} | Salário atual: R${:.2f} | percentual: {}% | valor do aumento:R${:.2f}'
+
+print(msg.format(*result))
+
+
+
+
+
+
+  
+
+
+
+    
+
+
+    
+
+
+
+    
+
+
+
