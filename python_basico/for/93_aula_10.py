1930933318b026fda2e5ada29b2e150c247dae7a
Fábio Florêncio <fabioflorencioxyz@gmail.com>
2024-05-22 21:48:06 -0300

    Complemento do estudo de for

---
+++ python_basico/for/93_aula_10.py
@@ -0,0 +1,30 @@
+'''
+    Exemplo usando for com a função reversed()
+    função reversed() em Python retorna um iterador que acessa os elementos de uma sequência em ordem inversa. 
+    Vamos ver um exemplo de como usar reversed() dentro de uma estrutura de repetição for.
+
+'''
+
+# Definindo uma lista de números
+numeros = [1, 2, 3, 4, 5]
+
+# Usando reversed() com a estrutura de repetição for
+for numero in reversed(numeros):
+    print(numero)
+
+
+invert = reversed(numeros)    
+
+print(invert)
+
+
+
+
+
+
+
+
+
+
+
+
