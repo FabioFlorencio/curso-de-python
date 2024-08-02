0b7c18a77c1b4c7616f0fbcc88c94bf7ee395423
Fábio Florêncio <fabioflorencioxyz@gmail.com>
2024-08-02 12:25:32 -0300

    Atualizacao do repositorio condicionais

--- python_basico/condicionais/35_aula_3.py
+++ python_basico/condicionais/35_aula_3.py
@@ -1,17 +1,19 @@
-
-idade1 = 14
+idade = 18
 genero = "Homem"
 valida_alistamento = False
+msg_parts= []
 
-if (valida_alistamento := (idade1 >= 16) & (genero == "Homem")):
-    print("Vai ser analisado seu alistamento.")
-    print('Retorne daqui 15 dias!')
+if (valida_alistamento := (idade >= 16) & (genero.casefold() == "homem")):
+    msg_parts.append("Vai ser analisado seu alistamento. Retorne daqui 15 dias!\n")    
 else:
-    print("Você não está apto ao alistamento.")
-
+    msg_parts.append("Você não está apto ao alistamento.")    
 
 if valida_alistamento:
-    print('Você foi convocado! Bem vindo ao exercito!')
+    msg_parts.append('Você foi convocado! Bem vindo ao exercito!')    
+
+
+msg = ''.join(msg_parts)
+print(msg)
 
 
 
